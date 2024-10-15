78146a80145de6fe0e01c7a99eccf569573a257d
Kornelia Rahayu <korneliarhyu2@gmail.com>
2024-10-15 14:56:08 +0700

    Adding Adapter (Still error)

--- app/src/main/java/com/example/booklibraryapp/MainActivity.java
+++ app/src/main/java/com/example/booklibraryapp/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.booklibraryapp;
 
 import java.util.ArrayList;
-import java.util.Currency;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,17 +20,19 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton add_button;
 
+
     DatabaseHelper myDB;
     ArrayList<String> book_id, book_title, book_author, book_pages;
+    CustomAdapter customAdapter;
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
         add_button = findViewById(R.id.add_button);
-        add_button.setOnClickListener(new View.OnClickListener() {
+        add_button.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick (View view) {
                 Intent intent = new Intent(MainActivity.this, AddActivity.class);
@@ -44,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
         book_pages = new ArrayList<>();
 
         storeDataInArrays();
+
+        customAdapter = new CustomAdapter(MainActivity.this, book_id, book_title, book_author, book_pages);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
     }
 
     void storeDataInArrays() {

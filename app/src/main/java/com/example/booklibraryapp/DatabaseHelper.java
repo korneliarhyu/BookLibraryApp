5dde26c5faf14e50df2fb7a5744a1e717f08e75c
Kornelia Rahayu <korneliarhyu2@gmail.com>
2024-10-17 20:54:55 +0700

    Adding Update (display done, database wont update)

--- app/src/main/java/com/example/booklibraryapp/DatabaseHelper.java
+++ app/src/main/java/com/example/booklibraryapp/DatabaseHelper.java
@@ -95,5 +95,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     }
 
+    void deleteOneRow(String row_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete(TABLE_NAME, "_id=?", new String[]{row_id});
+
+        if (result == -1) {
+            Toast.makeText(context, "Failed Delete.", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "Successfully Deleted.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/booklibraryapp/UpdateActivity.java
+++ app/src/main/java/com/example/booklibraryapp/UpdateActivity.java
@@ -1,5 +1,6 @@
 package com.example.booklibraryapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,11 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateActivity extends AppCompatActivity {
     EditText title_input, author_input, pages_input;
-    Button update_button;
+    Button update_button, delete_button;
     String id, title, author, pages;
 
     @Override
@@ -23,17 +26,40 @@ public class UpdateActivity extends AppCompatActivity {
         author_input = findViewById(R.id.author_input2);
         pages_input = findViewById(R.id.pages_input2);
         update_button = findViewById(R.id.update_button);
+        delete_button = findViewById(R.id.delete_button);
 
         getAndSetIntentData();
+
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setTitle(title);
+        }
 
         update_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Log.d("update button", title_input.getText().toString());
                 DatabaseHelper myDB = new DatabaseHelper(UpdateActivity.this);
-                myDB.updateData(id, title_input.getText().toString(), author_input.getText().toString(), pages_input.getText().toString());
+                title = title_input.getText().toString();
+                author = author_input.getText().toString();
+
+                myDB.updateData(id, title, author, pages);
             }
         });
+
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                confirmDialog();
+
+//                DatabaseHelper myDB = new DatabaseHelper(UpdateActivity.this);
+//                myDB.deleteOneRow(id);
+
+            }
+        });
+
+
 
     }
 
@@ -56,5 +82,26 @@ public class UpdateActivity extends AppCompatActivity {
         } else{
             Toast.makeText(this, "No Data", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    void confirmDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete " + title + " ?");
+        builder.setMessage("Are you sure you want to delete + " + title + " ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                DatabaseHelper myDB = new DatabaseHelper(UpdateActivity.this);
+                myDB.deleteOneRow(id);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.create().show();
     }
 }
